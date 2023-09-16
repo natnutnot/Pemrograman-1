fc058cf8ad77b3066f94f17d588f376f0a78a88f
natnutnot <144782256+natnutnot@users.noreply.github.com>
2023-09-16 18:18:22 +0800

    Add files via upload

---
+++ PRAK002-2310817120003-Natalie_Grace_Katiandagho.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main() {
+    printf("Selamat Pagi, Natalie Grace Katiandagho \n");
+    printf("Selamat Siang, Natalie Grace Katiandagho \n");
+    printf("Selamat Malam, Natalie Grace Katiandagho");
+}
---
+++ PRAK004-2310817120003-Natalie_Grace_Katiandagho.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("##################################### \n");
+    printf("#                                   # \n");
+    printf("#     Natalie Grace Katiandagho     # \n");
+    printf("#           2310817120003           # \n");
+    printf("#                                   # \n");
+    printf("#####################################");
+}
